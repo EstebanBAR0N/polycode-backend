71e9695895f4d8044363900f20f0137059b5cc2d
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-01 21:39:03 +0200

    Set up the main
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  //ensuring all endpoints are protected from receiving incorrect data.
+  // whitelist: remove extra fields, transform: automatically transform string to int (ex: id from request)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // protection from some well-known web vulnerabilities by setting HTTP headers appropriately
+  app.use(helmet());
+
+  // all routes start with /api/v1
+  app.setGlobalPrefix('api/v1');
+
+  const port = process.env.PORT || '4000';
+  await app.listen(port);
 }
 bootstrap();

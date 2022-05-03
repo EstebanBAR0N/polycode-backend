517e69f8157eb4bc9c2b96c088a9ced59e5f579a
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-03 09:28:56 +0200

    Clear Token route
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- src/routes/token/token.service.ts
+++ src/routes/token/token.service.ts
@@ -1,6 +1,4 @@
 import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
-import { CreateTokenDto } from './dto/create-token.dto';
-import { UpdateTokenDto } from './dto/update-token.dto';
 import { Token } from './entities/token.entity';
 
 @Injectable()
@@ -9,22 +7,6 @@ export class TokenService {
     @Inject('tokenModel')
     private tokenModel: typeof Token,
   ) {}
-
-  create(createTokenDto: CreateTokenDto) {
-    return 'This action adds a new token';
-  }
-
-  findAll() {
-    return `This action returns all token`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} token`;
-  }
-
-  update(id: number, updateTokenDto: UpdateTokenDto) {
-    return `This action updates a #${id} token`;
-  }
 
   async remove(tokenPk: string) {
     const token = await this.tokenModel.findByPk<Token>(tokenPk);

eb01a27dc9f95df0f15e6833d42be0ee729048b9
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-18 17:40:27 +0200

    (feat): first try implementing unit test
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- src/resources/user/test/user.controller.spec.ts
+++ src/resources/user/test/user.controller.spec.ts
@@ -1,43 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserDto } from '../dto/user.dto';
 import { User } from '../entities/user.entity';
+import { UserModule } from '../user.module';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
 
 describe('UserController', () => {
-  let userController: UserController;
+  let controller: UserController;
   let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
 
     userService = module.get<UserService>(UserService);
-    userController = module.get<UserController>(UserController);
+    controller = module.get<UserController>(UserController);
   });
 
   describe('findOne', () => {
-    it('should return some fields of the user', async () => {
-      const user = new User();
-      user.id = 'azajzbjansaza-azazvchbz-azazxczdza';
-      user.username = 'username';
-      user.email = 'username@gmail.com';
-      user.isEmailConfirmed = true;
-      user.emailConfirmationToken = 'azazfzea';
-      user.password = 'DASXas<jaazW>ZOEA';
-      user.isAdmin = false;
-
-      const userDto: UserDto = new UserDto(user);
-
-      jest
-        .spyOn(userService, 'findOne')
-        .mockImplementation(async () => await userDto);
-
-      expect(
-        await userController.findOne('azajzbjansaza-azazvchbz-azazxczdza', {}),
-      ).toBe(userDto);
-    });
+    // it('should return the user without password and email token', async () => {
+    //   const user = new User();
+    //   user.id = 'id';
+    //   user.username = 'username';
+    //   user.email = 'username@gmail.com';
+    //   user.isEmailConfirmed = true;
+    //   user.emailConfirmationToken = 'azazfzea';
+    //   user.password = 'DASXas<jaazW>ZOEA';
+    //   user.isAdmin = false;
+    //   const userDto: UserDto = new UserDto(user);
+    //   const expectedResult = userDto;
+    //   const spy = jest
+    //     .spyOn(userService, 'findOne')
+    //     .mockReturnValue(expectedResult);
+    //   // .mockImplementation(async (id: string, req: any) => await userDto);
+    //   const result = await controller.findOne('id', {});
+    //   expect(spy).toHaveBeenCalled();
+    //   expect(result).toBe(userDto);
+    // });
   });
 });

4716b18c1ad4fdf3350f695e4b943c67d1576c63
esteban <esteban.baron@etu.umontpellier.fr>
2022-05-18 17:41:18 +0200

    (feat): add ci and deploy.sh file
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # init ssh connection
-ssh esteban@162.38.112.131
+ssh -tt -o StrictHostKeyChecking=no esteban@162.38.112.131 << EOF
 
 # update polycode-backend repo
 cd ~esteban/polycode/polycode-backend
 git pull --rebase
 
 # deploy with docker compose file
-sudo su -
 cd ~esteban/polycode
 docker compose --env-file ./config/.env.prod up -d --build
 
-# back to esteban user
-exit
-
 # close ssh connection
 exit 
+
+EOF
